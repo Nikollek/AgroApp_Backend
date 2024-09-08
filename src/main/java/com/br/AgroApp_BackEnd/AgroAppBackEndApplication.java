125971ad8e929db2c601df4925e4a57217f2540f
nikollek <nikollekimberly@gmail.com>
2024-09-08 12:17:56 -0300

    esqueleto do projeto

---
+++ src/main/java/com/br/AgroApp_BackEnd/AgroAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.br.AgroApp_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroAppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroAppBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/AgroApp_BackEnd/AgroAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.AgroApp_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroAppBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
